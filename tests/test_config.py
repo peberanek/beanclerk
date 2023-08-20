a1d30230b6a81e7c94f32364b325be0451a94e12
Petr Beranek <petrberanek.mail@gmail.com>
2023-08-20 16:30:46 +0200

    test: fix test_load_importer

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,7 @@ import pydantic
 import pytest
 
 from beanclerk.config import Config, load_config, load_importer
-from beanclerk.importers.fio_banka import ApiImporter
+from beanclerk.importers import ApiImporterProtocol
 
 _valid_accounts = [
     {
@@ -109,4 +109,4 @@ def test_load_importer(config_file, ledger):
     config = load_config(config_file)
     for account_config in config.accounts:
         importer = load_importer(account_config)  # raises on invalid config
-        assert isinstance(importer, ApiImporter)
+        assert isinstance(importer, ApiImporterProtocol)
